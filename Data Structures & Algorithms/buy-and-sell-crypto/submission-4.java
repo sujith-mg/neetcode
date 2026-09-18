41b74c0b19e56226ee492f7496172d018e7ee419
sujith-mg <sujith7134@gmail.com>
2026-09-18 21:41:16 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int buyingprice=prices[0];
+        for(int i=0;i<prices.length;i++){
+            buyingprice=Math.min(buyingprice,prices[i]);
+            profit=Math.max(profit,prices[i]-buyingprice);
+
+        }
+        return profit;
+        
+    }
+}
